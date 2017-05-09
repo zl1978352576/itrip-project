ca7acfea095b6cd1214c5f502a998c42754c2baa
zezhong.shang <1044732267@qq.com>
2017-05-09 14:22:29 +0800

    代码生成器-生成分页

--- itripbiz/src/main/java/cn/itrip/controller/TestController.java
+++ itripbiz/src/main/java/cn/itrip/controller/TestController.java
@@ -2,16 +2,21 @@ package cn.itrip.controller;
 
 import cn.itrip.beans.dtos.Dto;
 import cn.itrip.beans.pojo.ItripAreaDic;
+import cn.itrip.beans.pojo.ItripComment;
+import cn.itrip.beans.pojo.ItripImage;
 import cn.itrip.common.Page;
+import cn.itrip.common.PropertiesUtils;
 import cn.itrip.service.itripAreaDic.ItripAreaDicService;
+import cn.itrip.service.itripComment.ItripCommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Created by zezhong.shang on 17-4-25.
@@ -21,7 +26,9 @@ import java.util.Map;
 public class TestController {
 
     @Autowired
-    private ItripAreaDicService iItripAreaDicService;
+    private ItripAreaDicService itripAreaDicService;
+
+    private ItripCommentService itripCommentService;
 
     @RequestMapping("/example")
     public void testExample() throws Exception {
@@ -30,7 +37,7 @@ public class TestController {
         itripAreaDic.setParent(new Long(1));
         itripAreaDic.setIsActivated(1);
         itripAreaDic.setIsHot(1);
-        int flag=iItripAreaDicService.itriptxAddItripAreaDic(itripAreaDic);
+        int flag=itripAreaDicService.itriptxAddItripAreaDic(itripAreaDic);
         System.out.println(flag);
     }
 
@@ -38,7 +45,7 @@ public class TestController {
     public void testSelect() throws Exception {
         Map<String,Object> param=new HashMap<String,Object>();
         param.put("id", "1");
-        List<ItripAreaDic> itripAreaDics=iItripAreaDicService.getItripAreaDicListByMap(param);
+        List<ItripAreaDic> itripAreaDics=itripAreaDicService.getItripAreaDicListByMap(param);
         for (ItripAreaDic itripAreaDic:itripAreaDics){
             System.out.println(itripAreaDic.getName());
         }
@@ -52,9 +59,57 @@ public class TestController {
         dto.setSuccess("true");
         dto.setErrorCode("0");
         Map<String,Object> param=new HashMap<String,Object>();
-        Page<ItripAreaDic> itripAreaDicPage=iItripAreaDicService.queryItripAreaDicPageByMap(param,2,5);
+        Page<ItripAreaDic> itripAreaDicPage=itripAreaDicService.queryItripAreaDicPageByMap(param, 2, 5);
         dto.setData(itripAreaDicPage);
         return  dto;
     }
+
+    @RequestMapping("/add")
+    public Dto<Page<ItripAreaDic>> add() throws Exception {
+        ItripComment comment = new ItripComment();
+        List<ItripImage> imageList = new ArrayList<ItripImage>();
+        ItripImage image = new ItripImage();
+        Date d = new Date();
+        String date = "2016-01-01";
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            d = df.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        image.setType("0");
+        image.setImgUrl("C:\\workspace");
+        image.setCreationDate(d);
+        comment.setHotelId(1L);
+        comment.setProductId(1L);
+        comment.setProductType(1);
+        comment.setUserId(1L);
+        comment.setIsHavingImg(1);
+        comment.setPositionScore(3);
+        comment.setFacilitiesScore(3);
+        comment.setServiceScore(4);
+        comment.setHygieneScore(3);
+        comment.setTripMode("1");
+        comment.setIsOk(1);
+        comment.setCreationDate(new Date());
+        comment.setCreatedBy(1l);
+        comment.setModifyDate(new Date());
+        comment.setModifiedBy(1l);
+        comment.setContent("1231231");
+        try {
+            Boolean flag = itripCommentService.itriptxAddItripComment(comment, imageList);
+            System.out.println("添加点评的数量为：" + flag);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            return  null;
+        }
+    }
+
+    public static void main(String[] args) {
+        String name= PropertiesUtils.get("validateNull.properties","api/hotel/queryHotCity");
+        System.out.println("name = [" + name + "]");
+    }
+
 }
 
--- itripbiz/src/main/java/cn/itrip/interceptor/ParamValidateInterceptor.java
+++ itripbiz/src/main/java/cn/itrip/interceptor/ParamValidateInterceptor.java
@@ -1,18 +1,64 @@
 package cn.itrip.interceptor;
+
+import cn.itrip.beans.dtos.Dto;
+import cn.itrip.common.DtoUtil;
+import cn.itrip.common.EmptyUtils;
+import cn.itrip.common.PropertiesUtils;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.log4j.Logger;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by zezhong.shang on 17-5-9.
  */
-public class ParamValidateInterceptor extends HandlerInterceptorAdapter{
+public class ParamValidateInterceptor extends HandlerInterceptorAdapter {
+
+    static Logger logger = Logger.getLogger(ParamValidateInterceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println(request.getRequestURL());
+        String reqUrl = request.getServletPath();
+        //验证当前接口是否需要进行非空判断
+        String nullValue = PropertiesUtils.get("validateNull.properties", reqUrl);
+        String regValue = PropertiesUtils.get("validateReg.properties", reqUrl);
+        logger.info(">>>>>>>>拦截到>>>>>>>>>请求:" + reqUrl + "验证的value为:" + nullValue);
+        if (EmptyUtils.isEmpty(nullValue)) {
+            return true;
+        }
+        //如果需要进行非空判断
+        String validateNullParam[] = nullValue.split(";")[0].split(",");
+        String validateNullMessage[] = nullValue.split(";")[1].split(",");
+        if (EmptyUtils.isNotEmpty(validateNullParam)) {
+            for (int i = 0; i < validateNullParam.length; i++) {
+                String temp = request.getParameter(validateNullParam[i]);
+                if (EmptyUtils.isEmpty(temp)) {
+                    reurnError("0001",validateNullMessage[i], response);
+                    return false;
+                }
+            }
+        }
+        //验证正则表达式
+        String validateParam[] = regValue.split(";")[0].split(",");
+        String validateRegAndMessage[] = regValue.split(";")[1].split(",");
+        if (EmptyUtils.isNotEmpty(validateParam)) {
+            for (int i = 0; i < validateParam.length; i++) {
+                String temp = request.getParameter(validateParam[i]);
+                Pattern pattern = Pattern.compile(validateRegAndMessage[i].split(":")[0]);
+                Matcher matcher = pattern.matcher(temp);
+                if (!matcher.matches()) {
+                    reurnError("0002",validateRegAndMessage[i].split(":")[1],response);
+                    return false;
+                }
+            }
+        }
         return true;
     }
 
@@ -24,5 +70,23 @@ public class ParamValidateInterceptor extends HandlerInterceptorAdapter{
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         super.afterCompletion(request, response, handler, ex);
+    }
+
+    private void reurnError(String errorCode,String message, HttpServletResponse response) {
+        response.setHeader("Content-type", "text/html;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        Dto dto = DtoUtil.returnFail(message, "0001");
+        PrintWriter writer = null;
+        try {
+            if (null != response) {
+                writer = response.getWriter();
+                writer.print(JSONObject.toJSONString(dto));
+                writer.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            writer.close();
+        }
     }
 }
