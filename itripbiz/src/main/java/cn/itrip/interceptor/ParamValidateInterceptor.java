c0b3f7de2fcb1ce09145213ec84d2f7514ae3d4e
zezhong.shang <1044732267@qq.com>
2017-05-09 10:11:34 +0800

    代码生成器-生成分页

---
+++ itripbiz/src/main/java/cn/itrip/interceptor/ParamValidateInterceptor.java
@@ -0,0 +1,28 @@
+package cn.itrip.interceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by zezhong.shang on 17-5-9.
+ */
+public class ParamValidateInterceptor extends HandlerInterceptorAdapter{
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println(request.getRequestURL());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        super.afterCompletion(request, response, handler, ex);
+    }
+}
