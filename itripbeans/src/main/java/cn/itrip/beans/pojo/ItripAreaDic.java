3e262130218bc6cf29021cb6ff8723ac5ff6c6b0
zezhong.shang <1044732267@qq.com>
2017-05-10 14:54:34 +0800

    酒店的接口

--- itripbeans/src/main/java/cn/itrip/beans/pojo/ItripAreaDic.java
+++ itripbeans/src/main/java/cn/itrip/beans/pojo/ItripAreaDic.java
@@ -2,112 +2,125 @@ package cn.itrip.beans.pojo;
 
 import java.io.Serializable;
 import java.util.Date;
+
 public class ItripAreaDic implements Serializable {
 
-            private Long id;
-            private String name;
-            private String areaNo;
-            private Long parent;
-            private Integer isActivated;
-            private Integer isTradingArea;
-            private Integer isHot;
-            private Integer level;
-            private Integer isChina;
-            private Date creationDate;
-            private Long createdBy;
-            private Date modifyDate;
-            private Long modifiedBy;
+    private Long id;
+    private String name;
+    private String areaNo;
+    private Long parent;
+    private Integer isActivated;
+    private Integer isTradingArea;
+    private Integer isHot;
+    private Integer level;
+    private Integer isChina;
+    private Date creationDate;
+    private Long createdBy;
+    private Date modifyDate;
+    private Long modifiedBy;
 
-            public void setId (Long  id){
-                this.id=id;
-            }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-            public  Long getId(){
-                return this.id;
-            }
-            public void setName (String  name){
-                this.name=name;
-            }
+    public Long getId() {
+        return this.id;
+    }
 
-            public  String getName(){
-                return this.name;
-            }
-            public void setAreaNo (String  areaNo){
-                this.areaNo=areaNo;
-            }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-            public  String getAreaNo(){
-                return this.areaNo;
-            }
-            public void setParent (Long  parent){
-                this.parent=parent;
-            }
+    public String getName() {
+        return this.name;
+    }
 
-            public  Long getParent(){
-                return this.parent;
-            }
-            public void setIsActivated (Integer  isActivated){
-                this.isActivated=isActivated;
-            }
+    public void setAreaNo(String areaNo) {
+        this.areaNo = areaNo;
+    }
 
-            public  Integer getIsActivated(){
-                return this.isActivated;
-            }
-            public void setIsTradingArea (Integer  isTradingArea){
-                this.isTradingArea=isTradingArea;
-            }
+    public String getAreaNo() {
+        return this.areaNo;
+    }
 
-            public  Integer getIsTradingArea(){
-                return this.isTradingArea;
-            }
-            public void setIsHot (Integer  isHot){
-                this.isHot=isHot;
-            }
+    public void setParent(Long parent) {
+        this.parent = parent;
+    }
 
-            public  Integer getIsHot(){
-                return this.isHot;
-            }
-            public void setLevel (Integer  level){
-                this.level=level;
-            }
+    public Long getParent() {
+        return this.parent;
+    }
 
-            public  Integer getLevel(){
-                return this.level;
-            }
-            public void setIsChina (Integer  isChina){
-                this.isChina=isChina;
-            }
+    public void setIsActivated(Integer isActivated) {
+        this.isActivated = isActivated;
+    }
 
-            public  Integer getIsChina(){
-                return this.isChina;
-            }
-            public void setCreationDate (Date  creationDate){
-                this.creationDate=creationDate;
-            }
+    public Integer getIsActivated() {
+        return this.isActivated;
+    }
 
-            public  Date getCreationDate(){
-                return this.creationDate;
-            }
-            public void setCreatedBy (Long  createdBy){
-                this.createdBy=createdBy;
-            }
+    public void setIsTradingArea(Integer isTradingArea) {
+        this.isTradingArea = isTradingArea;
+    }
 
-            public  Long getCreatedBy(){
-                return this.createdBy;
-            }
-            public void setModifyDate (Date  modifyDate){
-                this.modifyDate=modifyDate;
-            }
+    public Integer getIsTradingArea() {
+        return this.isTradingArea;
+    }
 
-            public  Date getModifyDate(){
-                return this.modifyDate;
-            }
-            public void setModifiedBy (Long  modifiedBy){
-                this.modifiedBy=modifiedBy;
-            }
+    public void setIsHot(Integer isHot) {
+        this.isHot = isHot;
+    }
 
-            public  Long getModifiedBy(){
-                return this.modifiedBy;
-            }
+    public Integer getIsHot() {
+        return this.isHot;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Integer getLevel() {
+        return this.level;
+    }
+
+    public void setIsChina(Integer isChina) {
+        this.isChina = isChina;
+    }
+
+    public Integer getIsChina() {
+        return this.isChina;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getCreationDate() {
+        return this.creationDate;
+    }
+
+    public void setCreatedBy(Long createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Long getCreatedBy() {
+        return this.createdBy;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    public Date getModifyDate() {
+        return this.modifyDate;
+    }
+
+    public void setModifiedBy(Long modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public Long getModifiedBy() {
+        return this.modifiedBy;
+    }
 
 }
