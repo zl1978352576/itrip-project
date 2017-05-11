b5245b0af8f700b9d76c7fcac92b933b4a8fda57
hduser <hduser@ly-PC>
2017-05-11 13:08:25 +0800

    定义错误代码

---
+++ itriputils/src/main/java/cn/itrip/common/ErrorCode.java
@@ -0,0 +1,9 @@
+package cn.itrip.common;
+
+public class ErrorCode {
+
+	public final static String AUTH_UNKNOWN="30000";
+	public final static String AUTH_USER_ALREADY_EXISTS="30001";//用户已存在
+	public final static String AUTH_AUTHENTICATION_FAILED="30002";//认证失败
+	public final static String AUTH_PARAMETER_ERROR="30003";//用户名密码参数错误，为空
+}
