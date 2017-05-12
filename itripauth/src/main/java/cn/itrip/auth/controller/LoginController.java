65d94ace695023dd50d04eb579fe3a87063853e8
hduser <hduser@ly-PC>
2017-05-12 10:09:41 +0800

    增加Swagger生成Controller API

--- itripauth/src/main/java/cn/itrip/auth/controller/LoginController.java
+++ itripauth/src/main/java/cn/itrip/auth/controller/LoginController.java
@@ -1,12 +1,18 @@
 package cn.itrip.auth.controller;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import springfox.documentation.annotations.ApiIgnore;
 import cn.itrip.auth.exception.UserLoginFailedException;
 import cn.itrip.auth.service.TokenService;
 import cn.itrip.auth.service.UserService;
@@ -25,12 +31,18 @@ public class LoginController {
 	@Resource
 	private TokenService tokenService;
 
+	@ApiIgnore
 	@RequestMapping("/login")
 	public String showLoginForm() {
 		return "login";
 	}
 
-	@RequestMapping("/dologin")
+	@ApiOperation(value = "用户登录",notes="根据用户名、密码进行统一认证")
+	@ApiImplicitParams({
+        @ApiImplicitParam(name = "name", value = "用户名(ItripUser.userCode)", required = true, dataType = "String"),
+        @ApiImplicitParam(name = "password", value = "密码(ItripUser.userPassword)", required = true, dataType = "String")
+	})
+	@RequestMapping(value="/dologin",method=RequestMethod.POST)
 	public @ResponseBody Dto dologin(String name, String password,
 			HttpServletRequest request) {
 		Dto dto = new Dto();
@@ -65,7 +77,7 @@ public class LoginController {
 		} else {
 			dto.setSuccess("true");
 			dto.setErrorCode(ErrorCode.AUTH_PARAMETER_ERROR);
-			dto.setMsg("参数错误！");
+			dto.setMsg("参数错误！检查提交的参数名称是否正确。");
 		}
 		return dto;
 	}
--- itripauth/src/main/java/cn/itrip/auth/controller/UserController.java
+++ itripauth/src/main/java/cn/itrip/auth/controller/UserController.java
@@ -1,13 +1,20 @@
 package cn.itrip.auth.controller;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 import java.util.Date;
 
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import springfox.documentation.annotations.ApiIgnore;
 import cn.itrip.auth.service.MailService;
 import cn.itrip.auth.service.UserService;
 import cn.itrip.auth.util.MD5;
@@ -20,12 +27,15 @@ public class UserController {
 	@Resource
 	private UserService userService;
 	
+	@ApiIgnore
 	@RequestMapping("/register")
 	public String showRegisterForm() {
 		return "register";
 	}
 
-	@RequestMapping("/doregister")
+	@ApiOperation(value="用户注册",notes="使用邮箱注册，参数示例:{\"属性名1\":\"属性值1\",\"属性名2\":\"属性值2\",……}")
+	@ApiImplicitParam(name="user",value="用户实体",required=true,dataType="ItripUser")
+	@RequestMapping(value="/doregister",method=RequestMethod.POST)
 	public @ResponseBody
 	Dto doRegister(ItripUser user) {
 		Dto dto = new Dto();
@@ -49,8 +59,9 @@ public class UserController {
 		}
 		return dto;
 	}
-
-	@RequestMapping("/ckusr")
+	@ApiOperation(value="用户验证",notes="验证是否已存在该用户名")
+	@ApiImplicitParam(name="name",value="用户注册名称",required=true,dataType="String")
+	@RequestMapping(value="/ckusr",method=RequestMethod.GET)
 	public @ResponseBody
 	Dto checkUser(String name) {
 		Dto dto = new Dto();
@@ -74,8 +85,13 @@ public class UserController {
 		return dto;
 	}
 	
-	@RequestMapping("/activate")
-	public @ResponseBody Dto activate(String user,String code){
+	@ApiOperation(value="注册用户激活",notes="邮箱激活")
+	@ApiImplicitParams({
+		@ApiImplicitParam(name="user",value="用户注册名",required=true,dataType="String"),
+		@ApiImplicitParam(name="code",value="激活码",required=true,dataType="String")
+	})	
+	@RequestMapping(value="/activate",method=RequestMethod.PUT)
+	public @ResponseBody Dto activate(String user,String code){	
 		Dto dto=new Dto();
 		try {
 			if(userService.activate(user, code))
