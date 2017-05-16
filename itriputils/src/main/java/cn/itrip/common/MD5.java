c2b20a07349ebd0621068538f930d05fa83cc2ed
lu.han <79460624@qq.com>
2017-05-16 14:52:40 +0800

    add MD5

---
+++ itriputils/src/main/java/cn/itrip/common/MD5.java
@@ -0,0 +1,42 @@
+package cn.itrip.common;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5 {
+
+	public static String getMd5(String plainText,int length) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(plainText.getBytes());
+			byte b[] = md.digest();
+
+			int i;
+
+			StringBuffer buf = new StringBuffer("");
+			for (int offset = 0; offset < b.length; offset++) {
+				i = b[offset];
+				if (i < 0)
+					i += 256;
+				if (i < 16)
+					buf.append("0");
+				buf.append(Integer.toHexString(i));
+			}
+			// 32位
+			// return buf.toString();
+			// 16位
+			// return buf.toString().substring(0, 16);
+			
+			return buf.toString().substring(0, length);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println(MD5.getMd5("helloadsfdsffsf",6));
+	}
+
+}
