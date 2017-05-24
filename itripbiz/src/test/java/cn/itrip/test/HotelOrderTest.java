0e8b76eb017bcf1460478ca43cac98d991795d00
hai.dong <sdodh366@163.com>
2017-05-24 09:57:35 +0800

    修改了订单总金额的计算

--- itripbiz/src/test/java/cn/itrip/test/HotelOrderTest.java
+++ itripbiz/src/test/java/cn/itrip/test/HotelOrderTest.java
@@ -18,12 +18,16 @@ public class HotelOrderTest {
     static {
 
     }
-
+    private  ItripHotelTempStoreService hotelTempStoreService;
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext-mybatis.xml");
+        hotelTempStoreService = (ItripHotelTempStoreService) context.getBean("itripHotelTempStoreServiceImpl");
+    }
     @Test
     public void test() throws Exception {
-        for (int i = 0; i < 40; i++) {
+        for (int i = 0; i < 400; i++) {
             //new 开辟子线程 start：调用run，运行子线程
-            new ThreadDemo("T" + i).start();
+            new ThreadDemo("T" + i, hotelTempStoreService).start();
             if (i == 1) {
                 try {
                     Thread.sleep(1000);
