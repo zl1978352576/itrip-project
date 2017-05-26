0f30e3d5ab788d7eb8ee6445a1f0663adc404fa8
zezhong.shang <1044732267@qq.com>
2017-05-26 11:19:34 +0800

    1.文档修改

--- itripsearch/src/main/java/cn/itrip/service/SearchHotelServiceImpl.java
+++ itripsearch/src/main/java/cn/itrip/service/SearchHotelServiceImpl.java
@@ -1,4 +1,5 @@
 package cn.itrip.service;
+
 import cn.itrip.beans.vo.hotel.ItripHotelVO;
 import cn.itrip.beans.vo.hotel.SearchHotelVO;
 import cn.itrip.common.EmptyUtils;
@@ -7,6 +8,7 @@ import cn.itrip.common.PropertiesUtils;
 import cn.itrip.dao.BaseQuery;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 /**
@@ -15,55 +17,79 @@ import java.util.List;
 @Service
 public class SearchHotelServiceImpl implements SearchHotelService {
 
-    public static String URL = PropertiesUtils.get("database.properties","baseUrl");
+    public static String URL = PropertiesUtils.get("database.properties", "baseUrl");
 
     private BaseQuery<ItripHotelVO> itripHotelVOBaseQuery = new BaseQuery(URL);
 
     @Override
     public Page<ItripHotelVO> searchItripHotelPage(SearchHotelVO vo, Integer pageNo, Integer pageSize) throws Exception {
         SolrQuery query = new SolrQuery("*:*");
-        StringBuffer tempQuery=new StringBuffer();
-        int tempFlag=0;
+        StringBuffer tempQuery = new StringBuffer();
+        int tempFlag = 0;
         if (EmptyUtils.isNotEmpty(vo)) {
             if (EmptyUtils.isNotEmpty(vo.getDestination())) {
-                tempQuery.append(" destination :"+vo.getDestination());
-                tempFlag=1;
+                tempQuery.append(" destination :" + vo.getDestination());
+                tempFlag = 1;
             }
             if (EmptyUtils.isNotEmpty(vo.getHotelLevel())) {
-                query.addFilterQuery("hotelLevel:"+vo.getHotelLevel()+"");
+                query.addFilterQuery("hotelLevel:" + vo.getHotelLevel() + "");
             }
             if (EmptyUtils.isNotEmpty(vo.getKeywords())) {
-                if(tempFlag==1){
-                    tempQuery.append(" AND keyword :"+vo.getKeywords());
-                }else{
-                    tempQuery.append(" keyword :"+vo.getKeywords());
+                if (tempFlag == 1) {
+                    tempQuery.append(" AND keyword :" + vo.getKeywords());
+                } else {
+                    tempQuery.append(" keyword :" + vo.getKeywords());
                 }
             }
             if (EmptyUtils.isNotEmpty(vo.getFeatureIds())) {
-                query.addFilterQuery("featureIds:"+"*," + vo.getFeatureIds() + ",*");
+                StringBuffer buffer = new StringBuffer("(");
+                int flag = 0;
+                String featureIdArray[] = vo.getFeatureIds().split(",");
+                for (String featureId : featureIdArray) {
+                    if (flag == 0) {
+                        buffer.append(" featureIds:" + "*," + featureId + ",*");
+                    } else {
+                        buffer.append(" OR featureIds:" + "*," + featureId + ",*");
+                    }
+                    flag++;
+                }
+                buffer.append(")");
+                query.addFilterQuery(buffer.toString());
             }
             if (EmptyUtils.isNotEmpty(vo.getTradeAreaIds())) {
-                query.addFilterQuery("tradingAreaIds:"+"*," + vo.getTradeAreaIds() + ",*");
+                StringBuffer buffer = new StringBuffer("(");
+                int flag = 0;
+                String tradeAreaIdArray[] = vo.getTradeAreaIds().split(",");
+                for (String tradeAreaId : tradeAreaIdArray) {
+                    if (flag == 0) {
+                        buffer.append(" tradingAreaIds:" + "*," + tradeAreaId + ",*");
+                    } else {
+                        buffer.append(" OR tradingAreaIds:" + "*," + tradeAreaId + ",*");
+                    }
+                    flag++;
+                }
+                buffer.append(")");
+                query.addFilterQuery(buffer.toString());
             }
             if (EmptyUtils.isNotEmpty(vo.getMaxPrice())) {
-                query.addFilterQuery("maxPrice:"+"[* TO " + vo.getMaxPrice() + "]");
+                query.addFilterQuery("minPrice:" + "[* TO " + vo.getMaxPrice() + "]");
             }
             if (EmptyUtils.isNotEmpty(vo.getMinPrice())) {
-                query.addFilterQuery("minPrice:"+"[" + vo.getMinPrice() + " TO *]");
+                query.addFilterQuery("minPrice:" + "[" + vo.getMinPrice() + " TO *]");
             }
 
             if (EmptyUtils.isNotEmpty(vo.getAscSort())) {
-                query.addSort(vo.getAscSort(),SolrQuery.ORDER.asc);
+                query.addSort(vo.getAscSort(), SolrQuery.ORDER.asc);
             }
 
             if (EmptyUtils.isNotEmpty(vo.getDescSort())) {
                 query.addSort(vo.getDescSort(), SolrQuery.ORDER.desc);
             }
         }
-        if(EmptyUtils.isNotEmpty(tempQuery.toString())){
+        if (EmptyUtils.isNotEmpty(tempQuery.toString())) {
             query.setQuery(tempQuery.toString());
         }
-        Page<ItripHotelVO> page= itripHotelVOBaseQuery.queryPage(query, pageNo, pageSize, ItripHotelVO.class);
+        Page<ItripHotelVO> page = itripHotelVOBaseQuery.queryPage(query, pageNo, pageSize, ItripHotelVO.class);
         return page;
     }
 
@@ -71,7 +97,7 @@ public class SearchHotelServiceImpl implements SearchHotelService {
     public List<ItripHotelVO> searchItripHotelListByHotCity(Integer cityId, Integer count) throws Exception {
         SolrQuery query = new SolrQuery("*:*");
         if (EmptyUtils.isNotEmpty(cityId)) {
-            query.addFilterQuery("cityId:"+cityId);
+            query.addFilterQuery("cityId:" + cityId);
         } else {
             return null;
         }
